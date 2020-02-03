21a817792b0b2f6bb8c095ac70435fa749a038d4
Leticia.cardozo <leticia.cardozo@satc.edu.br>
2020-02-03 13:54:24 -0300

    login

---
+++ src/pages/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <q-page padding>
+        <q-form v-on:keyup.enter="logar()" :model="fomularioLogin" ref="formularioLogin" class="q-gutter-md">
+            <q-input filled lazy-rules type="email" v-model="fomularioLogin.email" label="Email"
+                :rules="[val => val && val.length > 0 || 'Insira o E-mail']">
+            </q-input>
+            <q-input filled type="password" lazy-rules v-model="formularioLogin.senha" label="Senha"
+                :rules="[val => val && val.length > 0 || 'Insira a senha']">
+            </q-input>
+
+            <div class="row justify-center content-center">
+                <q-btn label="Logar" @click="logar()" color="primary">
+                    <q-tooltip
+                        transition-show="rotate"
+                        transition-hide="rotate">
+                        Logar!
+                    </q-tooltip>
+                </q-btn>
+
+                <q-btn label="Limpar" @click="limpar()" color="primary" flar class="q-ml-sm">
+                    <q-tooltip
+                        transition-show="rotate"
+                        transition-hide="rotate">
+                        Limpar Formulário!
+                    </q-tooltip>
+                </q-btn>
+            </div>
+        </q-form>
+    </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formularioLogin: {}
+    }
+  },
+  methods: {
+    logar () {
+
+    },
+    limpar () {
+      this.formularioLogin = {}
+    }
+  },
+  create () {
+    console.log('Assim que a tela for iniciada essa mensagem irá aparecer')
+  }
+}
+</script>
